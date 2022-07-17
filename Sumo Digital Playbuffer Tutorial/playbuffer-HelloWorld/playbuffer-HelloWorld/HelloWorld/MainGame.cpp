dae3e05b724e9b405a10e73767feac239bd8ce1a
Katherine Russell <71149848+KatRuss@users.noreply.github.com>
2022-07-17 15:11:27 +0100

    Spider movement

--- Sumo Digital Playbuffer Tutorial/playbuffer-HelloWorld/playbuffer-HelloWorld/HelloWorld/MainGame.cpp
+++ Sumo Digital Playbuffer Tutorial/playbuffer-HelloWorld/playbuffer-HelloWorld/HelloWorld/MainGame.cpp
@@ -6,36 +6,72 @@ int DISPLAY_WIDTH = 1280;
 int DISPLAY_HEIGHT = 720;
 int DISPLAY_SCALE = 1;
 
+const char background[] = "Data\\Backgrounds\\background.png";
+const char music[] = "music";
+
 struct GameState {
-	float timer = 0;
-	int spriteID = 0;
+	int score = 0;
+
 };
 
 GameState gameState;
+
+enum GameObjectType {
+	TYPE_NULL = -1,
+	TYPE_AGENT8,
+};
+
+void HandlePlayerControls() {
+	GameObject& obj_agent8 = Play::GetGameObjectByType(TYPE_AGENT8);
+	if (Play::KeyDown(VK_UP))
+	{
+		obj_agent8.acceleration = { 0,-2 };
+		Play::SetSprite(obj_agent8, "agent8_climb", 0.02f);
+	}
+	else if (Play::KeyDown(VK_DOWN))
+	{
+		obj_agent8.acceleration = { 0,1 };
+		Play::SetSprite(obj_agent8, "agent8_fall", 0.02f);
+	}
+	else
+	{
+		Play::SetSprite(obj_agent8, "agent8_hang", 0.02f);
+		obj_agent8.velocity *= 0.5f;
+		obj_agent8.acceleration = { 0,0 };
+	}
+	Play::UpdateGameObject(obj_agent8);
+
+	if (Play::IsLeavingDisplayArea(obj_agent8))
+	{
+		obj_agent8.pos = obj_agent8.oldPos;
+	}
+
+	Play::DrawLine({ obj_agent8.pos.x,0 }, obj_agent8.pos, Play::cWhite);
+	Play::DrawObjectRotated(obj_agent8);
+
+}
+
 
 // The entry point for a PlayBuffer program
 void MainGameEntry( PLAY_IGNORE_COMMAND_LINE )
 {
 	Play::CreateManager( DISPLAY_WIDTH, DISPLAY_HEIGHT, DISPLAY_SCALE );
+	Play::CentreAllSpriteOrigins();
+	Play::LoadBackground(background);
+	//Play::StartAudioLoop(music);
+	Play::CreateGameObject(TYPE_AGENT8, { 115,0 }, 50, "agent8");
 }
 
 // Called by PlayBuffer every frame (60 times a second!)
 bool MainGameUpdate( float elapsedTime )
 {
-	gameState.timer += elapsedTime;
-	Play::ClearDrawingBuffer( Play::cOrange );
-	Play::DrawDebugText( { DISPLAY_WIDTH / 2, DISPLAY_HEIGHT / 2 }, Play::GetSpriteName(gameState.spriteID),Play::cWhite);
-	
-	Play::DrawSprite(gameState.spriteID, Play::GetMousePos(), gameState.timer);
-
-	if (Play::KeyPressed(VK_SPACE))
-	{
-		gameState.spriteID++;
-	}
-	
-	Play::PresentDrawingBuffer();
-	return Play::KeyDown( VK_ESCAPE );
+	Play::ClearDrawingBuffer( Play::cOrange ); //Start of graphics buffer for a given frame 
+	Play::DrawBackground();
+	HandlePlayerControls();
+	Play::PresentDrawingBuffer(); //End of Graphics buffer for a given frame
+	return Play::KeyDown( VK_ESCAPE ); 
 }
+
 
 // Gets called once when the player quits the game 
 int MainGameExit( void )
