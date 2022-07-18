1cda2d02c9e8f417cfc705176e1a17ae413f787f
Katherine Russell <71149848+KatRuss@users.noreply.github.com>
2022-07-18 12:10:28 +0100

    Coins and Stars

--- Sumo Digital Playbuffer Tutorial/playbuffer-HelloWorld/playbuffer-HelloWorld/HelloWorld/MainGame.cpp
+++ Sumo Digital Playbuffer Tutorial/playbuffer-HelloWorld/playbuffer-HelloWorld/HelloWorld/MainGame.cpp
@@ -57,7 +57,6 @@ void HandlePlayerControls() {
 
 }
 
-
 void UpdateFan() {
 	GameObject& obj_fan = Play::GetGameObjectByType(TYPE_FAN);
 	
@@ -127,9 +126,70 @@ void UpdateTools() {
 }
 
 void UpdateCoinsAndStars() {
+
+	GameObject& obj_agent8 = Play::GetGameObjectByType(TYPE_AGENT8);
+
+	std::vector<int> vCoins = Play::CollectGameObjectIDsByType(TYPE_COIN);
 	
+	for (int id_coin : vCoins)
+	{
+		GameObject& obj_coin = Play::GetGameObject(id_coin);
+		bool hasCollided = false;
+
+		if (Play::IsColliding(obj_coin,obj_agent8))
+		{
+			for (float rad{0.25f}; rad < 2.0f;  rad += 0.5f)
+			{
+				int id = Play::CreateGameObject(TYPE_STAR,obj_agent8.pos,0,"star");
+				GameObject& obj_star = Play::GetGameObject(id);
+				obj_star.rotSpeed = 0.1f;
+				obj_star.acceleration = { 0.0f,0.5f };
+				Play::SetGameObjectDirection(obj_star, 16, rad * PLAY_PI);
+			}
+
+			hasCollided = true;
+			gameState.score += 500;
+			Play::PlayAudio("collect");
+		}
+
+		Play::UpdateGameObject(obj_coin);
+		Play::DrawObjectRotated(obj_coin);
+
+		if (!Play::IsVisible(obj_coin) || hasCollided)
+		{
+			Play::DestroyGameObject(id_coin);
+		}
+	}
+
+	std::vector<int> vStars = Play::CollectGameObjectIDsByType(TYPE_STAR);
+
+	for (int id_star: vStars)
+	{
+		GameObject& obj_star = Play::GetGameObject(id_star);
+
+		Play::UpdateGameObject(obj_star);
+		Play::DrawObjectRotated(obj_star);
+
+		if (!Play::IsVisible(obj_star))
+		{
+			Play::DestroyGameObject(id_star);
+		}
+	}
+
+
 }
 
+void UpdateLasers() {
+
+}
+
+void UpdateDestroyed() {
+
+}
+
+
+
+//---------------Game Loop--------------------
 // The entry point for a PlayBuffer program
 void MainGameEntry( PLAY_IGNORE_COMMAND_LINE )
 {
@@ -151,11 +211,12 @@ bool MainGameUpdate( float elapsedTime )
 	HandlePlayerControls();
 	UpdateFan();
 	UpdateTools();
+	UpdateLasers();
+	UpdateDestroyed();
 	UpdateCoinsAndStars();
 	Play::PresentDrawingBuffer(); //End of Graphics buffer for a given frame
 	return Play::KeyDown( VK_ESCAPE ); 
 }
-
 
 // Gets called once when the player quits the game 
 int MainGameExit( void )
